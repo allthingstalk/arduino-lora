191817a49d1e68cedcea9b856df8c8531f3a78d8
Jan Bogaerts <jb@allthingstalk.com>
2015-10-27 16:56:18 +0100

    gps module is working

--- ATT_Lora_IOT/examples/sensor tests/GPS/GPS.ino
+++ ATT_Lora_IOT/examples/sensor tests/GPS/GPS.ino
@@ -33,85 +33,96 @@ void setup()
   Serial.println("Ready to send data");
 }
 
-float longitude = 1.1;
-float latitude = 2.2;
-float altitude = 3.3;
-float timestamp = 3.3;
+float longitude;
+float latitude;
+float altitude;
+float timestamp;
 
 void loop() 
 {
-  readCoordinates();
-  //SendValue();
+  if(readCoordinates() == true)
+    SendValue();
   delay(300);
 }
 
 void SendValue()
 {
-  Device.Queue(longitude);
-  Device.Queue(latitude);
-  Device.Queue(altitude);
-  Device.Queue(timestamp);
-  Device.Send(GPS);
+  //Device.Queue(longitude);
+  //Device.Queue(latitude);
+  //Device.Queue(altitude);
+  //Device.Queue(timestamp);
+  //Device.Send(GPS);
+  Serial.print("lng: ");
+  Serial.print(longitude, 4);
+  Serial.print(", lat: ");
+  Serial.print(latitude, 4);
+  Serial.print(", alt: ");
+  Serial.print(altitude);
+  Serial.print(", time: ");
+  Serial.println(timestamp);
 }
 
-void readCoordinates()
+bool readCoordinates()
 {
+    bool foundGPGGA = false;					//sensor can return multiple types of data, need to capture lines that start with $GPGGA
 	if (SoftSerial.available())                     // if date is coming from software serial port ==> data is coming from SoftSerial shield
-    {
-		bool foundGPGGA = false;					//sensor can return multiple types of data, need to capture lines that start with $GPGGA
-		while(foundGPGGA == false)
-        {
-			while(SoftSerial.available())               // reading data into char array
-			{
-				buffer[count++]=SoftSerial.read();      // writing data into array
-				if(count == 64)break;
-			}
-			Serial.write(buffer,count);                 // if no data transmission ends, write buffer to hardware serial port
-			foundGPGGA = ExtractValues();
-			clearBufferArray();                         // call clearBufferArray function to clear the stored data from the array
-			count = 0;                                  // set counter of while loop to zero
+	{
+		while(SoftSerial.available())               // reading data into char array
+		{
+			buffer[count++]=SoftSerial.read();      // writing data into array
+			if(count == 64)break;
 		}
-    }
-	
+		//Serial.println(count);
+		foundGPGGA = count > 60 && ExtractValues();  //if we have less then 60 characters, then we have bogus input, so don't try to parse it or process the values
+		clearBufferArray();                         // call clearBufferArray function to clear the stored data from the array
+		count = 0;                                  // set counter of while loop to zero
+	}
+	return foundGPGGA;
 }
 
 bool ExtractValues()
 {
 	unsigned char start = count;
-	while(start >= 0 && buffer[start] != '$')		//find the start of the GPS data -> multiple $GPGGA can appear in 1 line, if so, need to take the last one.
-		start--;
-	start++;										//remove the '$', don't need to compare with that.
-	if(strcmp(buffer + start, "GPGGA") == 0)		//we found the correct line, so extract the values.
+	while(buffer[start] != '$')		//find the start of the GPS data -> multiple $GPGGA can appear in 1 line, if so, need to take the last one.
 	{
-		unsigned char end;
-		timestamp = ExtractValue(start, end);
-		start = end + 1;
-		longitude = ExtractValue(start, end);
-		end = Skip(start + 1);
-		latitude = ExtractValue(start, end);
-		end = Skip(start + 1);
-		end = Skip(start + 1);
-		end = Skip(start + 1);
-		altitude = ExtractValue(start, end);
-		start = end + 1;
+	    if(start == 0) break;						//it's unsigned char, so we can't check on <= 0
+		start--;
+	}
+	start++;										//remove the '$', don't need to compare with that.
+	if(start + 4 < 64 && buffer[start] == 'G' && buffer[start+1] == 'P' && buffer[start+2] == 'G' && buffer[start+3] == 'G' && buffer[start+4] == 'A')		//we found the correct line, so extract the values.
+	{
+        start+=6;
+		timestamp = ExtractValue(start);
+		longitude = ExtractValue(start) / 100;
+		start = Skip(start);	
+		latitude = ExtractValue(start)  / 100;
+		start = Skip(start);
+		start = Skip(start);
+		start = Skip(start);
+		start = Skip(start);
+		altitude = ExtractValue(start);
 		return true;
 	}
 	else
 		return false;
 }
 
-float ExtractValue(unsigned char start, unsigned char& end)
+float ExtractValue(unsigned char& start)
 {
-	end = start + 1;
+	unsigned char end = start + 1;
 	while(end < count && buffer[end] != ',')		//find the start of the GPS data -> multiple $GPGGA can appear in 1 line, if so, need to take the last one.
 		end++;
 	buffer[end] = 0;								//end the string, so we can create a string object from the sub string -> easy to convert to float.
-	return String(buffer + start).toFloat();
+	float result = 0.0;
+	if(end != start + 1)					//if we only found a ',', then there is no value.
+		result = String((const char*)(buffer + start)).toFloat();
+	start = end + 1;
+	return result;
 }
 
 unsigned char Skip(unsigned char start)
 {
-	end = start + 1;
+	unsigned char end = start + 1;
 	while(end < count && buffer[end] != ',')		//find the start of the GPS data -> multiple $GPGGA can appear in 1 line, if so, need to take the last one.
 		end++;
 	return end+1;
