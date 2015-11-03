0e55bbf1fa7effda7d8f4a2fbc63e6438734cad7
Jan Bogaerts <jb@allthingstalk.com>
2015-11-03 14:23:28 +0100

    modem ref update in test

--- ATT_Lora_IOT/examples/container tests/PUSH_BUTTON/PUSH_BUTTON.ino
+++ ATT_Lora_IOT/examples/container tests/PUSH_BUTTON/PUSH_BUTTON.ino
@@ -8,14 +8,14 @@ Original author: Jan Bogaerts (2015)
 #include <Wire.h>
 #include "ATT_Lora_IOT.h"
 #include "keys.h"
-#include "EmbitLoRaModem.h"
-//#include "MicrochipLoRaModem.h"
+//#include "EmbitLoRaModem.h"
+#include "MicrochipLoRaModem.h"
 
 #define SERIAL_BAUD 57600
 
 int DigitalSensor = 20;                                        // Digital Sensor is connected to pin D8 on grove shield 
-EmbitLoRaModem Modem(&Serial1);
-//MicrochipLoRaModem Modem(&Serial1);
+//EmbitLoRaModem Modem(&Serial1);
+MicrochipLoRaModem Modem(&Serial1);
 ATTDevice Device(&Modem);
 
 
